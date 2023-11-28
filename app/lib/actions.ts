369d67d2711c44a53bddb222af54adb22f8756c2
mikemitrofanov <mitrofanov.mihail@gmail.com>
2023-11-28 19:08:17 +0100

    Crete invoice functionality

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,5 +1,37 @@
 'use server';
 
-export async function createInvoice(formData: FormData) {
+import { PrismaClient } from '@prisma/client';
+import { z } from 'zod';
+import { revalidatePath } from 'next/cache';
+import { redirect } from 'next/navigation';
+import { CustomersTable } from "./definitions";
 
+const prisma: PrismaClient = new PrismaClient();
+const FormSchema = z.object({
+  id: z.string(),
+  customerId: z.string(),
+  amount: z.coerce.number(),
+  status: z.enum(['pending', 'paid']),
+  date: z.string(),
+});
+
+const CreateInvoice = FormSchema.omit({ id: true, date: true });
+
+export async function createInvoice(formData: FormData) {
+  const rawFormData = Object.fromEntries(formData.entries())
+  const { customerId, amount, status } = CreateInvoice.parse(rawFormData);
+  const amountInCents = amount * 100;
+  
+  await prisma.invoices.create({
+    data: {
+      customer_id: customerId,
+      amount: amountInCents,
+      status,
+      date: new Date().toISOString()
+    }
+  })
+  await prisma.$disconnect();
+  
+  revalidatePath('/dashboard/invoices');
+  redirect('/dashboard/invoices');
 }
